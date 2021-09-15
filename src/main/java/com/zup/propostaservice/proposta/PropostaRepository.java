7bf532fcaaad9df414e8ba098f69120876d44a65
tales-zup <tales.araujo@zup.com.br>
2021-09-15 15:34:54 -0300

    proposta repository

---
+++ src/main/java/com/zup/propostaservice/proposta/PropostaRepository.java
@@ -0,0 +1,6 @@
+package com.zup.propostaservice.proposta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropostaRepository extends JpaRepository<Proposta, Long> {
+}
